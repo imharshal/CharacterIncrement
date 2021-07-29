0d4cb5d0dc1d523189a75880a84ce14ccf10b058
Harshal Bokade <harshalbokade984@gmail.com>
2021-07-29 12:55:41 +0530

    Add project files.

---
+++ CharacterIncrement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CharacterIncrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str,strNew;
+            strNew = "";
+            int i, l;
+            i = l = 0;
+            Console.WriteLine("Enter a string");
+            str = Console.ReadLine();
+            l = str.Length;
+
+            while (i < l)
+            {
+                strNew += Convert.ToChar( str[i] + 1);
+                i++;
+            }
+            Console.WriteLine(strNew);
+
+        }
+    }
+}
